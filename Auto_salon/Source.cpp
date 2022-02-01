b9157e7cb54105c6ac6b87d411be6e257eb22e1c
Dmytro <=>
2022-02-01 14:49:44 +0200

    added salon

--- Auto_salon/Source.cpp
+++ Auto_salon/Source.cpp
@@ -4,7 +4,35 @@
 
 using namespace std;
 
-class automobile;
+class automobile
+{
+	string name;
+	int year;
+	double engine;
+	int price;
+public:
+	automobile()
+	{
+		name = "NULL";
+		year = 0;
+		engine = 0;
+		price = 0;
+	}
+	automobile(string name, int year, double engine, int price)
+	{
+		this->name = name;
+		this->year = year;
+		this->engine = engine;
+		this->price = price;
+	}
+	void print()
+	{
+		cout << "Name: " << name << "\n";
+		cout << "Year: " << year << "\n";
+		cout << "Engine: " << engine << "\n";
+		cout << "Price: " << price << "\n";
+	}
+};
 
 
 
